979a9c394415fa1f09574a096d0b14e40a8a7af4
marcel631 <marcel63@hotmail.com>
2017-02-08 22:02:03 +0100

    Update _write.c

--- _write.c
+++ _write.c
@@ -1,7 +1,21 @@
-//
-// This file is part of the µOS++ III distribution.
-// Copyright (c) 2014 Liviu Ionescu.
-//
+/**	
+ * |----------------------------------------------------------------------
+ * | Copyright (C) SMI Holding BV, www.smidev.nl, 2017
+ * | 
+ * | This program is free software: you can redistribute it and/or modify
+ * | it under the terms of the GNU General Public License as published by
+ * | the Free Software Foundation, either version 3 of the License, or
+ * | any later version.
+ * |  
+ * | This program is distributed in the hope that it will be useful,
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * | GNU General Public License for more details.
+ * | 
+ * | You should have received a copy of the GNU General Public License
+ * | along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * |----------------------------------------------------------------------
+ */
 
 // Do not include on semihosting and when freestanding
 #if !defined(OS_USE_SEMIHOSTING) && !(__STDC_HOSTED__ == 0)
